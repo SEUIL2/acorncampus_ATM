5c94dd584c27643d28a337bcab98e5b211d0f3cb
hyunkyumlee <103377141+hyunkyumlee@users.noreply.github.com>
2026-03-25 16:29:58 +0900

    현겸작업
    1. Account 엔티티에 toString, islogined 추가
    2. AccountAuthService 인터페이스, handlePasswordError 매개변수 타입 변경
    3. AccountAuthServiceImp 서비스 클래스 구현

--- acroncampus_ATM/src/main/java/com/service/interfacePackage/AccountAuthService.java
+++ acroncampus_ATM/src/main/java/com/service/interfacePackage/AccountAuthService.java
@@ -11,5 +11,5 @@ public interface AccountAuthService {
     // 3. 사용자 인증 및 보안 [cite: 16]
     boolean login(String accountNo, String password); // 3.1 로그인 체크 [cite: 17]
     void logout(); // 3.2 로그아웃 및 세션 종료 [cite: 18]
-    void handlePasswordError(String accountNo); // 3.3 비밀번호 오류 횟수 카운트 및 계좌 잠금 [cite: 19]
+    void handlePasswordError(Account account); // 3.3 비밀번호 오류 횟수 카운트 및 계좌 잠금 [cite: 19]
 }
