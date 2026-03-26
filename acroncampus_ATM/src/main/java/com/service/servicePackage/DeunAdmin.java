d0793e15936fbeb1260426fa8c8bbf38ac5196b9
doniie41 <qhbztrjccv@privaterelay.appleid.com>
2026-03-26 14:54:09 +0900

    해든작업
    매서드 완성, 테스트 완료

--- acroncampus_ATM/src/main/java/com/service/servicePackage/DeunAdmin.java
+++ acroncampus_ATM/src/main/java/com/service/servicePackage/DeunAdmin.java
@@ -1,8 +1,10 @@
 package com.service.servicePackage;
 
+import com.entity.Account;
 import com.entity.AdminLog;
 import com.entity.AtmMachine;
 import com.service.interfacePackage.AdminService;
+import com.service.servicePackage.transaction.TransactionBalanceService;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -10,14 +12,20 @@ import java.util.List;
 
 public class DeunAdmin implements AdminService {
 
-    private AtmMachine atmMachine = new AtmMachine(1000000000);  //10억
-
-    public DeunAdmin(AtmMachine atmMachine) {
-        this.atmMachine = atmMachine;
-    }
+    private AtmMachine atmMachine;
+    private List<Account> accounts;
+    private TransactionBalanceService transactionBalanceService;
 
     public DeunAdmin() {
+        this.atmMachine = new AtmMachine(1000000000);  // 10억
+        this.accounts = new ArrayList<>();
+    }
 
+    public DeunAdmin(AtmMachine atmMachine, List<Account> accounts,
+                     TransactionBalanceService transactionBalanceService) {
+        this.atmMachine = atmMachine;
+        this.accounts = accounts;
+        this.transactionBalanceService = transactionBalanceService;
     }
 
     @Override
@@ -42,7 +50,7 @@ public class DeunAdmin implements AdminService {
     }
 
     @Override
-    public boolean adminLogin(String adminPassword) {   // 관리자 로그인 6
+    public boolean adminLogin(String adminPassword) {   // 관리자 로그인 4
         // 비번은 1234 임
         String password = "1234";
         if (adminPassword.equals(password)){
@@ -54,14 +62,31 @@ public class DeunAdmin implements AdminService {
 
     private final List<AdminLog> adminLogs = new ArrayList<>();
 
+    public List<AdminLog> getAdminLogs() {
+        return adminLogs;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
     @Override
-    public void recordAdminLog(String type, long amount, LocalDateTime timestamp) {   // 4 관리자 로그 기계 자체에서 입출금 기록
+    public void recordAdminLog(String type, long amount, LocalDateTime timestamp) {   // 5 관리자 로그 기계 자체에서 입출금 기록
         adminLogs.add(new AdminLog(type, amount, timestamp));
     }
 
     @Override
-    public List<String> viewAllTransactionLogs() {  // 영석 같이 5, 기계 자체 거래 내역 확인
-        return List.of();
+    public List<String> viewAllTransactionLogs() {  // 영석 같이 6 기계 고객 거래 전체 로그 내역 확인
+        List<String> viewAll = new ArrayList<>();
+        for (int i = 0 ; i<accounts.size(); i++) {
+            Account account = accounts.get(i);      // 계좌 목록에서 계좌를 뽑아냄
+            String accountNo = account.getAccountNo();  // 계좌에서 계좌번호를 뽑아냄
+
+            List<String> viewAccount = transactionBalanceService.getRecentHistory(accountNo);// 뽑아낸 계좌번호로 계좌 기록을 뽑아냄
+
+            viewAll.addAll(viewAccount);
+        }
+        return viewAll;
     }
 
 
